765420ae3dabfaf599e8162c599066912061af82
Ondřej Topínka <topetopinka7@seznam.cz>
2026-07-15 16:13:40 +0200

    Dalsi pokus o opravu spatnych zavislosti

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import icon from "astro-icon";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
 export default defineConfig({
     site: "https://travasstineni.cz",
-
-    integrations: [react(), icon()],
+    integrations: [react(), icon(), tailwind()],
     output: "static",
-
-    vite: {
-        plugins: [tailwindcss(), tsconfigPaths()],
-    },
 });
